b30f4f91fbd9545550c36943af3826c75e2ad713
sehirsig <sebastianhirsig@bluewin.ch>
2022-04-08 21:00:02 +0200

    Event-Based Simulation gedebuggt und getestet. Ausgabe mit Statistik vollständig und funktionierend.

--- PythonEinfuehrung/PythonEinfuehrung.py
+++ PythonEinfuehrung/PythonEinfuehrung.py
@@ -1,15 +1,4 @@
 import heapq
-from threading import Lock, Thread
-import time
-
-#Idee
-# Nur Kunden rufen die Ereignisliste auf, neue Ereignisse.
-# Die Stationen bekommen wir aus der Liste des Kunden, und können von da deren Methoden aufrufen
-
-
-queuePushLock = Lock()
-queuePopLock = Lock()
-
 
 # Konstanten für Ereignisse
 CUSTOMER_ENTRANCE = 0
@@ -17,26 +6,11 @@ CUSTOMER_ARRIVE = 1
 STATION_QUEUE = 2
 STATION_FINISHED = 3
 
-#zeitSkalierung = 0.05 # 1 = normal -> 0.5 doppelt so schnell
-
-#class timer:
-#    def __init__(self):
-#        self.__t = Thread(target=self.__routine__)
-#
-#    zeit = 0
-#    def __routine__(self):
-#        while (self.zeit < supermarkt.maxSimulationsZeit):
-#            time.sleep(1 * zeitSkalierung)
-#            self.zeit = self.zeit + 1
-#
-#    def start(self):
-#        self.__t.start()
 
 class Ereignisliste:
     maxSimulationsZeit = 0
     maxEreignisnummer = 0
     ereignisnummer = 0
-    uhr = None
 
     def __init__(self, s, e):
         self.maxSimulationsZeit = s
@@ -44,59 +18,38 @@ class Ereignisliste:
         self.queue = []
 
     def pop(self):
-        queuePopLock.acquire()
         e = heapq.heappop(self.queue)
-        queuePopLock.release()
         return e
 
     def push(self, a):
-        queuePushLock.acquire()
         heapq.heappush(self.queue, a)
-        queuePushLock.release()
 
     def isEmpty(self):
         return len(self.queue) == 0
 
     def start(self):  # Startet die Simulation
-        #self.uhr = timer()
         self.zeit = 0
-        #self.uhr.start()
         while not self.isEmpty():
 
-            if (self.zeit == self.maxSimulationsZeit): # Wenn maximale Simulationszeit erreicht, beenden.
-                print("Time over.")
-                print("-----------------------")
-                print("Rest:")
-                while not self.isEmpty():
-                    popped = self.pop()
-                    print(popped)
-
-                return;
-
-            if (self.ereignisnummer == self.maxEreignisnummer):
+            if (self.ereignisnummer == self.maxEreignisnummer): #Wenn die vorgegebene Ereignisnummer erreicht ist, abbrechen.
                 print("Ereignisnummer maximum reached.")
                 print("-----------------------")
                 print("Rest:")
                 while not self.isEmpty():
                     popped = self.pop()
+                    #Die noch verbliebenen Events per print zeigen.
                     print(popped)
+                statistikAuswerten()
                 return;
 
-            d, e, f, g, caller = self.pop() #(ereigniszeitpunkt, ereignispriorität, ereignisnummer, ereignisfunktion, ereignisargument)
-
+            d, e, f, g, caller = self.pop()  # (ereigniszeitpunkt, ereignispriorität, ereignisnummer, ereignisfunktion, ereignisargument)
 
             if (self.zeit != d):
                 while d > self.zeit:
-                    if (self.zeit == self.maxSimulationsZeit):  # Wenn maximale Simulationszeit erreicht, beenden.
-                        print("Rest:")
-                        while not self.isEmpty():
-                            popped = self.pop()
-                            print(popped)
-                        return;
                     self.zeit += 1
-                    #time.sleep(0.45 * zeitSkalierung)
 
-            if (isinstance(caller, KundIn)): # Wenn der Aufrufer ein KundIn ist. Muss immer Kunde sein, sonnst klappt heapQueue nicht!
+            if (isinstance(caller,
+                           KundIn)):  # Wenn der Aufrufer ein KundIn ist. Muss immer Kunde sein, sonnst klappt heapQueue nicht!
                 if g == CUSTOMER_ENTRANCE:
                     caller.beginn_einkauf()
                 elif g == CUSTOMER_ARRIVE:
@@ -109,115 +62,173 @@ class Ereignisliste:
                     station_temp.fertig(caller)
                     caller.verlassen_station()
 
+            if (self.isEmpty()): # Wenn Eventliste fertig ist, alles auswerten.
+                statistikAuswerten()
+                return;
+
 
 class KundIn:
     name = ""
     liste = list()
     curStation = None
-    curBediendauer = 0
     curT = 0
     curW = 0
     curN = 0
 
-    #Statistiken
-    kompletteBedienzeit = 0
+    # Statistiken
+    kompletteEinkaufszeit = 0
+    uebersprungeneStationen = 0
 
-    def __init__(self, stationenTupleListe, name):  # Liste aus Tuplen mit List((Station, T, W, N), (Station, T, W, N), ... )
+    def __init__(self, stationenTupleListe,
+                 name):  # Liste aus Tuplen mit List((Station, T, W, N), (Station, T, W, N), ... )
         self.liste = list(stationenTupleListe)
         self.name = name
 
-
     def beginn_einkauf(self):  # Ereignis kreiren
         if self.liste:  # Wenn Liste nicht Leer ist
-            self.kompletteBedienzeit = time.time()
             self.curStation, self.curT, self.curW, self.curN = self.liste.pop(0)
-            self.curBediendauer = self.curStation.bediendauer
-            print(str(supermarkt.zeit) + "::  " "Kunde " + str(self.name) + " beginnt den Einkauf um " + str(supermarkt.zeit))
-            #print("Station: " + str(self.curStation.name)) # Abarbeitung, Eintrag in Eventliste wann nächste Ankunft
-            #print("T: " + str(self.curT)) # Zeit T die der Kunde benötigt um von Station y zu Station x zu kommen.
-            #print("W: " + str(self.curW)) # Ab welcher Wartenschlangenlänge W der Kunde die Station x auslässt
-            #print("N: " + str(self.curN)) # Anzahl der Einkäufe N, die Kunde an Station x tätigt.
-            #print("Bediendauer: " + str(self.curBediendauer))
-            print("-----------------------")
+            self.kompletteEinkaufszeit = supermarkt.zeit
 
             supermarkt.ereignisnummer += 1
             neuesEreignis = (supermarkt.zeit + self.curT, 3, supermarkt.ereignisnummer, CUSTOMER_ARRIVE, self)
             supermarkt.push(neuesEreignis)
 
-    def ankunft_station(self):  # Ereignis kreiren
-        self.liste = self.liste  # Abarbeitung, Eintrag in Eventliste wann Zuende
-        print(str(supermarkt.zeit) + "::  " + str(self.name) + " um " + str(supermarkt.zeit) + " an Station " + str(self.curStation.name) + " angekommen")
-        print("-----------------------")
-        # Stationsmethode aufrufen um anzustellen, oder wenn zuviel angestellt weiter springen
-        if (len(self.curStation.queue) <= int(self.curW)): #überprüfen ob queue nicht zu lange
+    def ankunft_station(self):
+        #Statistik
+        self.curStation.anzahlDerKunden += 1
+
+        # überprüfen ob queue nicht zu lange, false => überspringen
+        if len(self.curStation.queue) + 1 <= int(self.curW):
+            #Neues Event, welches angibt, dass der Kunde sich jetzt sofort in die Queue einreiht.
             supermarkt.ereignisnummer += 1
             neuesEreignis = (supermarkt.zeit, 1, supermarkt.ereignisnummer, STATION_QUEUE, self)
             supermarkt.push(neuesEreignis)
+
+            # Statistik und LOG
+            output_customer.append(f"{supermarkt.zeit}:{self.name} Queueing at {self.curStation.name}")
         else:
+            # Wenn Queue zu lange, überspringen und die nächste Station ansteuern
+
+            #Statistik und LOG
+            self.curStation.anzahlAusgelassen += 1
+            self.uebersprungeneStationen += 1
+            output_customer.append(f"{supermarkt.zeit}:{self.name} Dropped at {self.curStation.name}")
+
+            # Wenn alle Stationen besucht sind, den Supermarkt verlassen. Die Zeit stoppen, um die Einkaufszeit festzuhalten.
+            if len(self.liste) == 0:
+                self.kompletteEinkaufszeit = supermarkt.zeit - self.kompletteEinkaufszeit
+                return;
+
+            #Neue Station holen
             self.curStation, self.curT, self.curW, self.curN = self.liste.pop(0)
-            self.curBediendauer = self.curStation.bediendauer
+
+            #Neues Ereignis, wann der Kunde bei der Station ankommt
             supermarkt.ereignisnummer += 1
             neuesEreignis = (supermarkt.zeit + self.curT, 3, supermarkt.ereignisnummer, CUSTOMER_ARRIVE, self)
-            print(str(supermarkt.zeit) + "::  " + str(self.name) + " geht jetzt um " + str(
-                supermarkt.zeit) + " zur Station " + str(self.curStation.name))
-            print("-----------------------")
-            supermarkt.push(neuesEreignis)#auslassen und nächste station ansteuern.
+            supermarkt.push(neuesEreignis)
 
-    def verlassen_station(self):  # Ereignis kreiren, Eintrag in Eventliste wann nächste Ankunft
+    def verlassen_station(self):
+        output_customer.append(f"{supermarkt.zeit}:{self.name} Finished at {self.curStation.name}")
         if (len(self.liste) == 0):
-            print(str(supermarkt.zeit) + "::  " + str(self.name) + " um " + str(supermarkt.zeit) + " verlässt den Supermarkt.")
-            print("-----------------------")
+            # Wenn alle Stationen besucht sind, den Supermarkt verlassen. Die Zeit stoppen, um die Einkaufszeit festzuhalten.
+            self.kompletteEinkaufszeit = supermarkt.zeit - self.kompletteEinkaufszeit
         else:
             self.curStation, self.curT, self.curW, self.curN = self.liste.pop(0)
-            self.curBediendauer = self.curStation.bediendauer
+
+            #Neues Event, wann der Kunde an diese nächste Station ankommen wird.
             supermarkt.ereignisnummer += 1
             neuesEreignis = (supermarkt.zeit + self.curT, 3, supermarkt.ereignisnummer, CUSTOMER_ARRIVE, self)
-            print(str(supermarkt.zeit) + "::  " + str(self.name) + " geht jetzt um " + str(
-                supermarkt.zeit) + " zur Station " + str(self.curStation.name))
-            print("-----------------------")
-            supermarkt.push(neuesEreignis)  # auslassen und nächste station ansteuern.
+            supermarkt.push(neuesEreignis)
 
+#Station Status
+NOTBUSY = 0
+BUSY = 1
 
 class Station:
     queue = list()
     bediendauer = 0
     name = ""
+    status = 0
+
+    # Statistik
+    anzahlAusgelassen = 0
+    anzahlDerKunden = 0
 
     def __init__(self, bediendauer, name):
         self.bediendauer = bediendauer
         self.name = name
+        self.queue = list()
+        self.status = NOTBUSY
 
-    def anstellen(self, KundIn):  # Add to queue
+    def isEmpty(self):
+        return len(self.queue) == 0
+
+    def anstellen(self, KundIn):
+        #In der Queue einreihen
         self.queue.append(KundIn)
-        print(str(supermarkt.zeit) + "::  " + str(KundIn.name) + " stellt sich um " + str(supermarkt.zeit) + " bei " + str(self.name) + " als " + str(len(self.queue)) + " an.")
-        print("-----------------------")
-        supermarkt.ereignisnummer += 1
-        neuesEreignis = (supermarkt.zeit + (self.bediendauer * int(KundIn.curN)), 2, supermarkt.ereignisnummer, STATION_FINISHED, KundIn)
-        supermarkt.push(neuesEreignis)
-        #Ereignis wann der Kunde bedient wird.
+
+        #LOG
+        output_station.append(f"{supermarkt.zeit}:{self.name} adding customer {KundIn.name}")
+
+        if self.status == NOTBUSY:
+            #LOG
+            output_station.append(f"{supermarkt.zeit}:{self.name} serving customer {KundIn.name}")
+
+            #Status auf BUSY setzen
+            self.status = BUSY
+
+            # Ereignis wann der Kunde bedient wird.
+            supermarkt.ereignisnummer += 1
+            neuesEreignis = (
+            supermarkt.zeit + (self.bediendauer * int(KundIn.curN)), 2, supermarkt.ereignisnummer, STATION_FINISHED,
+            KundIn)
+            supermarkt.push(neuesEreignis)
 
     def fertig(self, KundIn):
-        # Abarbeitung
-        self.queue.pop(0)
-        print(str(supermarkt.zeit) + "::  " + str(KundIn.name) + " wurde um " + str(
-            supermarkt.zeit) + " bei " + str(self.name) + " fertig bedient")
-        print("-----------------------")
+        # Variable customer initialisieren
+        customer = KundIn
+        # Wenn die Liste nicht leer ist, den Kunden nach FIFO-Policy poppen
+        if not self.isEmpty():
+            customer = self.queue.pop(0)
+
+        #Status auf NOTBUSY stellen
+        self.status = NOTBUSY
+
+        #LOG
+        output_station.append(f"{supermarkt.zeit}:{self.name} finished customer {KundIn.name}")
+
+        #Wenn die Kiste nicht leer ist und der gepopte Kunde nicht derselbe vom Parameter ist, weitergehen.
+        if not self.isEmpty() or customer != KundIn:
+            # Wenn der custommer der Kunde aus dem Parameter ist, neu poppen.
+            if customer == KundIn:
+                customer = self.queue.pop(0)
+
+            #Status auf Busy setzen
+            self.status = BUSY
+
+            #Statistik
+            output_station.append(f"{supermarkt.zeit}:{self.name} serving customer {customer.name}")
+
+            #Neues Ereignis, welches angibt wann der Kunde fertig bedient wurde.
+            supermarkt.ereignisnummer += 1
+            neuesEreignis = (
+            supermarkt.zeit + (self.bediendauer * int(customer.curN)), 2, supermarkt.ereignisnummer, STATION_FINISHED,
+            customer)
+            supermarkt.push(neuesEreignis)
 
 
-
-
-#Supermark initialisieren
-maximalZeit = 350
-maximalEreignis = 300
-supermarkt = Ereignisliste(maximalZeit,maximalEreignis)
+# Supermark initialisieren
+maximalZeit = 1800
+maximalEreignis = 1000000
+supermarkt = Ereignisliste(maximalZeit, maximalEreignis)
 
 # Stationen
 # Bäcker
 baecker = Station(10, "Bäcker")
 # Wursttheke
-wursttheke = Station(30, "Wursttheke")
+wursttheke = Station(30, "Metzger")
 # Käsetheke
-kaesetheke = Station(60, "Käsetheke")
+kaesetheke = Station(60, "Käse")
 # Kasse
 kasse = Station(5, "Kasse")
 
@@ -242,33 +253,88 @@ typ2.append((wursttheke, 30, 5, 2))
 typ2.append((kasse, 30, 20, 3))
 typ2.append((baecker, 20, 20, 3))
 
+# Statistiken
+liste_aller_kunden = list()
+
+
+def getKundenAnzahl():
+    return len(liste_aller_kunden)
+
+#Listen für Customer/Station Output
+output_customer = list()
+output_station = list()
+
 
 def customerSpawner(maxZeit):
     abstandTyp1 = 200
     abstandTyp2 = 60
     startTyp1 = 0
-    startTyp2= 1
+    startTyp2 = 1
     anzahlTyp1 = 0
     anzahlTyp2 = 0
-    while ((int(startTyp1)) < int(maxZeit)):
+
+    #Event für Kunden vom Typ 1 festlegen.
+    while ((int(startTyp1)) <= int(maxZeit)):
         supermarkt.ereignisnummer += 1
         anzahlTyp1 += 1
-        typ1Kunde = KundIn(typ1, f"Kunde {anzahlTyp1} Typ 1")
+        typ1Kunde = KundIn(typ1, f"A{anzahlTyp1}")
         # Ereignis = (ereigniszeitpunkt, ereignispriorität, ereignisnummer, ereignisfunktion, ereignisargument)
         # Ein Ereignis ist ein 5-Tupel
         neuesEreignis = (startTyp1, supermarkt.ereignisnummer, 1, CUSTOMER_ENTRANCE, typ1Kunde)
         startTyp1 += abstandTyp1
         supermarkt.push(neuesEreignis)
+        liste_aller_kunden.append(typ1Kunde)
 
-    while ((int(startTyp2)) < int(maxZeit)):
+    #Event für Kunden vom Typ 2 festlegen.
+    while ((int(startTyp2)) <= int(maxZeit)):
         supermarkt.ereignisnummer += 1
         anzahlTyp2 += 1
-        typ2Kunde = KundIn(typ2, f"Kunde {anzahlTyp2} Typ 2")
+        typ2Kunde = KundIn(typ2, f"B{anzahlTyp2}")
         neuesEreignis = (startTyp2, supermarkt.ereignisnummer, 1, CUSTOMER_ENTRANCE, typ2Kunde)
         startTyp2 += abstandTyp2
         supermarkt.push(neuesEreignis)
+        liste_aller_kunden.append(typ2Kunde)
 
 
+def statistikAuswerten():
+
+    #supermarkt_customer Output
+    for output in output_customer:
+        print(output)
+
+    #supermarkt_station Output
+    for output in output_station:
+        print(output)
+
+    anzahl_vollstaendiger_einkaufe = 0
+    liste_aller_kunden.sort(key=lambda x: x.kompletteEinkaufszeit)
+
+    mittlere_einkaufsdauer = 0
+    mittlere_einkaufsdauer_vollstaendig = 0
+
+    for customer in liste_aller_kunden:
+        if (customer.uebersprungeneStationen == 0):
+            anzahl_vollstaendiger_einkaufe += 1
+            mittlere_einkaufsdauer_vollstaendig = mittlere_einkaufsdauer_vollstaendig + customer.kompletteEinkaufszeit
+        mittlere_einkaufsdauer = mittlere_einkaufsdauer + customer.kompletteEinkaufszeit
+
+    mittlere_einkaufsdauer = mittlere_einkaufsdauer / getKundenAnzahl()
+    mittlere_einkaufsdauer_vollstaendig = mittlere_einkaufsdauer_vollstaendig / anzahl_vollstaendiger_einkaufe
+
+    droppercentageBaecker = baecker.anzahlAusgelassen * 100 /baecker.anzahlDerKunden if baecker.anzahlAusgelassen != 0 else 0
+    droppercentageWursttheke = wursttheke.anzahlAusgelassen * 100 / wursttheke.anzahlDerKunden if wursttheke.anzahlAusgelassen != 0 else 0
+    droppercentageKaesetheke = kaesetheke.anzahlAusgelassen * 100 / kaesetheke.anzahlDerKunden if kaesetheke.anzahlAusgelassen != 0 else 0
+    droppercentageKasse = kasse.anzahlAusgelassen * 100 / kasse.anzahlDerKunden if kasse.anzahlAusgelassen != 0 else 0
+
+    print(f"\nSimulationsende: {supermarkt.zeit}s")
+    print(f"Anzahl Kunden: {len(liste_aller_kunden)}")
+    print(f"Anzahl vollständige Einkäufe: {anzahl_vollstaendiger_einkaufe}")
+    print(f"Mittlere Einkaufsdauer: %.2fs" % mittlere_einkaufsdauer)
+    print(f"Mittlere Einkaufsdauer (vollständig): %.2fs" % mittlere_einkaufsdauer_vollstaendig)
+    print(f"Drop percentage at Bäcker: %.2f" % droppercentageBaecker)
+    print(f"Drop percentage at Wursttheke: %.2f" % droppercentageWursttheke)
+    print(f"Drop percentage at Käsetheke: %.2f" % droppercentageKaesetheke)
+    print(f"Drop percentage at Kasse: %.2f" % droppercentageKasse)
 
 if __name__ == '__main__':
     customerSpawner(maximalZeit)
