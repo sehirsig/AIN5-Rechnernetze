77c312efe0542a3520bec971664b2905616d670f
Johannes Wirbser <johannes.wirbser@gmx.de>
2022-05-09 15:44:11 +0200

    Aufgabe4 full implemented TCP

---
+++ Einstieg in die Socketprogrammierung/Aufgabe4Client.py
@@ -0,0 +1,26 @@
+import socket
+import struct
+import time
+
+Server_IP = '127.0.0.1'
+Server_PORT = 50000
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.settimeout(10)
+print('Connecting to TCP server with IP ', Server_IP, ' on Port ', Server_PORT)
+sock.connect((Server_IP, Server_PORT))
+
+try:
+    print("Operator angeben: 1 - 4 (Summe, Produkt, Minimum, Maximum)")
+    op = int(input())
+    l = input().split(" ")
+    l = list(map(int, l))
+    s1 = struct.pack("ii", op, len(l))
+    sock.send(s1)
+    s2 = struct.pack("i"*len(l), *l)
+    sock.send(s2)
+    res = struct.unpack("l", sock.recv(8))
+    print("Ergebnis: " + str(res[0]))
+except socket.timeout:
+    print('Socket timed out at',time.asctime())
+sock.close()
