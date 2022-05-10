4bfe5a489354df834ff90cf33c2123beeb663e54
sehirsig <sebastianhirsig@bluewin.ch>
2022-05-10 15:22:37 +0200

    Aufgabe 5 update.

--- Einstieg in die Socketprogrammierung/Aufgabe5.py
+++ Einstieg in die Socketprogrammierung/Aufgabe5.py
@@ -10,16 +10,18 @@ def connectThem(number):
     sock.settimeout(10)
     # print('TCP Client created')
     Server_IP = '141.37.168.26'
-    Server_PORT = i
+    Server_PORT = number
+    print(Server_PORT)
     #print('Connecting to TCP server with IP ', Server_IP, ' on Port ', Server_PORT)
     try:
         sock.connect((Server_IP, Server_PORT))
+        print("connected")
         msg = "Hello"
         sock.send(msg)
         answer = sock.recv(1024)
         print("Ergebnis Socket " + i + ": " + str(answer))
-    except socket.timeout:
-        pass
+    except:
+        print(str(number) + " Failed.")                            
         #print('Socket timed out at', time.asctime())
     sock.close()
 
