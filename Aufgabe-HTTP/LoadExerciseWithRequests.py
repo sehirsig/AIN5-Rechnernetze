70478a24649983d3f65437939eae776c5cac06a1
Johannes Wirbser <johannes.wirbser@gmx.de>
2022-06-20 16:29:08 +0200

    other attempt

--- Aufgabe-HTTP/LoadExerciseWithRequests.py
+++ Aufgabe-HTTP/LoadExerciseWithRequests.py
@@ -8,10 +8,10 @@ password = input()
 
 server = 'https://moodle.htwg-konstanz.de/moodle/'
 session = requests.session()
-response = session.get(server)
-site = response.text
-moodleSession = response.cookies.get("MoodleSession")
-session_cookie = 'MoodleSession: "' + moodleSession + '"'
+response1 = session.get(server)
+site = response1.text
+moodleSession = response1.cookies.get("MoodleSession")
+session_cookie = {'MoodleSession': moodleSession}
 
 
 def search_login_token():
@@ -19,11 +19,12 @@ def search_login_token():
     LENGTH = 32
     return site[start_pos: start_pos + LENGTH]
 
-login = "username=" + username + "&password=" + password + "&logintoken=" + search_login_token()
-response = session.post(server, login, cookies=session_cookie)
 
-pdf_url = server + 'pluginfile.php/346660/mod_assign/introattachment/0/AIN%20RN%20-%20Laboraufgabe%20-%20HTTP.pdf?forcedownload=1'
-response = session.get(pdf_url, cookies=session_cookie, headers=session.headers)
-file = response.content
+login = "username=" + username + "&password=" + password + "&logintoken=" + search_login_token()
+response2 = session.post(server, login, cookies=session_cookie, allow_redirects=False)
+
+pdf_url = server + 'pluginfile.php/346660/mod_assign/introattachment/0/AIN%20RN%20-%20Laboraufgabe%20-%20HTTP.pdf'
+response2 = session.get(pdf_url, cookies=session_cookie, allow_redirects=False)
+file = response2.content
 
 session.close()
